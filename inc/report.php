ed785a42c2ebfb66e541be585a25715833c9b8f6
Joni Halabi <joni.halabi@georgetown.edu>
2019-07-02 12:09:40 -0400

    Updates from a while ago.

--- inc/report.php
+++ inc/report.php
@@ -1,51 +1,47 @@
 <?php
 
 // For reference: This code uses http://simplehtmldom.sourceforge.net/manual.htm
-require('simplehtmldom_1_5/simple_html_dom.php');
-
+require( 'simplehtmldom_1_5/simple_html_dom.php' );
 
 function init() {
-  $sites = file('inc/sites.txt');
-  foreach ($sites as $site) {
-    runReport($site);
+  $sites = file( 'inc/sites.txt' );
+  foreach ( $sites as $site ) {
+    runReport( $site );
   }
 }
 
-
-/* ---
+/**
  * function getSitemap()
  *   Gets the XML sitemap of a given site.
  *   Returns an object.
  */
-
-function getSitemap($site) {
-  $url = 'http://' . trim($site) . '.georgetown.edu/';
-  $sitemap = @file_get_contents($url . 'sitemap.xml');
-  return simplexml_load_string($sitemap);
+function getSitemap( $site ) {
+  $url = 'https://' . trim( $site ) . '.georgetown.edu/sitemap.xml';
+  $sitemap = file_get_contents( $url) ;
+  return simplexml_load_string( $sitemap );
 }
 
 
-/* ---
+/**
  * function runReport()
  *   Runs the report (finds code) for a given site.
  *   Prints out the report if it exists.
  */
-
-function runReport($site) {
-  $xml = getSitemap($site);
-  if (!$xml) { return false; }
+function runReport( $site ) {
+  $xml = getSitemap( $site );
+  if ( ! $xml ) { return false; }
 
   $printedSiteName = false;
-  $context = stream_context_create(array('http' => array( 'timeout' => 1200, )));
+  $context = stream_context_create( array( 'http' => array( 'timeout' => 1200, ) ) );
 
-  foreach ($xml->url as $url) {
-    $html = file_get_html($url->loc, false, $context);
+  foreach ( $xml->url as $url ) {
+    $html = file_get_html( $url->loc, false, $context );
 
-    if ($html) {
-      $code = findCode($html);
+    if ( $html ) {
+      $code = findCode( $html );
 
-      if ($code) {
-        if (!$printedSiteName) {
+      if ( $code ) {
+        if ( ! $printedSiteName ) {
           print '<h2>' . $site . '</h2>';
           $printedSiteName = true;
         }
@@ -57,26 +53,23 @@ function runReport($site) {
   }
 }
 
-
-/* ---
+/**
  * function findCode()
  *   Finds the code for a specific given selector on a given page.
  *   Returns HTML of all instances of that code, if it exists.
  */
-
-function findCode($html) {
+function findCode( $html ) {
   $code = '';
 
-  foreach ($html->find('main iframe, main script, main object, main form') as $element) {
+  foreach ( $html->find( 'main iframe, main script, main object, main form' ) as $element ) {
     $code .= '<tr>';
     $code .= '<td>' . $element->tag . '</td>';
-    $code .= '<td><code>' . htmlspecialchars($element) . '</code></td>';
+    $code .= '<td><code>' . htmlspecialchars( $element ) . '</code></td>';
     $code .= '</tr>';
   }
 
   return $code;
 }
-
 
 // Run this thing!
 init();
--- index.php
+++ index.php
@@ -9,17 +9,20 @@
   <body>
     <header>
       <h1>Georgetown Site Reports</h1>
+
       <p>
         This report lists all instances of <code>&lt;script&gt;</code>, <code>&lt;iframe&gt;</code>,
         <code>&lt;object&gt;</code>, and <code>&lt;form&gt;</code> tags in the main content area of
         any Georgetown site.
       </p>
+
       <p>
         Report created: <?php print date('d F Y'); ?>
+      </p>
     </header>
 
     <main>
-      <?php require('inc/report.php'); ?>
+      <?php require( 'inc/report.php' ); ?>
     </main>
   </body>
 </html>
